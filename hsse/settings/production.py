cd6d234eb6aa448bec210b274d94239b03e836ae
navicester <navicester@qq.com>
2017-09-16 08:52:36 +0800

    add db

--- hsse/settings/production.py
+++ hsse/settings/production.py
@@ -8,7 +8,7 @@ DATABASES = settings.DATABASES
 
 import dj_database_url
 # Parse database configuration from $DATABASE_URL
-DATABASES['default'] =  dj_database_url.config()
+DATABASES['default'] =  dj_database_url.config(default=mysql://bhe001:steer101214@nafuzyyfvaoi.mysql.sae.sina.com.cn:10238/sinotrans)
 
 # Allow all host headers
 ALLOWED_HOSTS = ['*']
