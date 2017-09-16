08a6a9589d3ac67f883f76a345c7650f11bb4e1c
navicester <navicester@qq.com>
2017-09-16 09:37:44 +0800

    update

--- hsse/settings/production.py
+++ hsse/settings/production.py
@@ -2,8 +2,8 @@ import os
 from django.conf import settings
 import settings_security
 
-DEBUG = False
-TEMPLATE_DEBUG = False
+DEBUG = True
+TEMPLATE_DEBUG = True
 
 DATABASES = settings.DATABASES
 
