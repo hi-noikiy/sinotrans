70d8718604db5cd381b452eb1e9e6c55a53afa92
navicester <navicester@qq.com>
2017-09-25 22:33:49 +0800

    update

---
+++ static_in_env/static_root/js/custom.js
@@ -0,0 +1,17 @@
+function showFlashMessage(message, id="") {
+	// var template = "{% include 'alert.html' with message='" + message + "' %}"
+	var template = "<div class='container container-alert-flash'>" + 
+	"<div class='col-sm-3 col-sm-offset-8'> " + 
+	"<div class='alert alert-success alert-dismissible' role='alert'>" + 
+	"<button type='button' class='close' data-dismiss='alert' aria-label='Close'>" +
+	"<span aria-hidden='true'>&times;</span></button>" 
+	+ message + "</div></div></div>"
+	if(id){
+		$(id).append(template);}else{
+		$("body").append(template);	
+	}
+	$(".container-alert-flash").fadeIn();
+	setTimeout(function(){ 
+		$(".container-alert-flash").fadeOut();
+	}, 1800);
+}
