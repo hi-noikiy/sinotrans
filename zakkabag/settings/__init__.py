519eb9bd2d9f738f8414eb9efa5f16e60ec04314
navicester <navicester@qq.com>
2017-10-17 16:15:18 +0800

    clean #2

--- zakkabag/settings/__init__.py
+++ zakkabag/settings/__init__.py
@@ -1,14 +1,20 @@
 from .base import *
 from django.conf import settings
 
+try:
+    import local.py
+    sqlite = True
+except:
+    pass
+
+sqlite = False    
 heroku = False
 mysql = True
 sae = False
 
-
-if 'SERVER_SOFTWARE' in os.environ or settings.USE_SAE_DB == True: 
-	from .sae import *
-elif sae:
+if sqlite:
+    pass
+elif 'SERVER_SOFTWARE' in os.environ or sae == True: 
 	from .sae import *
 elif mysql:
 	from .mysql import *	
