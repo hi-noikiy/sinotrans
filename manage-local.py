fee344f523460671dbc50efbc03b33badb6b4743
hordechief <hordechief@qq.com>
2017-10-17 23:17:22 +0800

    update

--- manage-local.py
+++ manage-local.py
@@ -6,7 +6,7 @@ import socket
 if __name__ == "__main__":
     root = os.path.dirname(__file__)
     sys.path.insert(0, os.path.join(root, 'site-packages-pip-fail'))
-	sys.path.insert(0, os.path.join(root, 'site-packages-djangos','1.9'))
+    sys.path.insert(0, os.path.join(root, 'site-packages-djangos','1.9'))
     sys.path.insert(0, os.path.join(root, 'site-packages-upgrade-stop'))
     sys.path.insert(0, os.path.join(root, 'site-packages'))
     if not 'SERVER_SOFTWARE' in os.environ:         
